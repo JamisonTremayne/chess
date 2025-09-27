cb0d38c6951b2c771b9274a8e07dfd94cb4088e7
JamisonTremayne <jamisontremayne@gmail.com>
2025-09-27 15:32:14 -0600

    Generated methods
    Generated methods for equals(), hashCode(), and toString().

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -205,5 +206,27 @@ public class ChessGame {
         }
 
         return validMoves.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return currentTurn == chessGame.currentTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentTurn, board);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessGame{" +
+                "currentTurn=" + currentTurn +
+                ", board=" + board +
+                '}';
     }
 }
